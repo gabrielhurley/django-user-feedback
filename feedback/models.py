1a144b501cbce13ca6aaa685b4a3df14030d1d9a
Gabriel Hurley <gabriel@strikeawe.com>
2011-06-08 17:06:52 -0700

    Adding a type field to the Feedback model.

--- feedback/models.py
+++ feedback/models.py
@@ -1,5 +1,12 @@
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import User
+
+FEEDBACK_TYPES = (
+    ('bug', 'Bug'),
+    ('feature', 'Feature'),
+    ('support', 'Support request'),
+)
 
 class Feedback(models.Model):
     feedback = models.TextField()
@@ -8,6 +15,7 @@ class Feedback(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True, editable=False)
     resolved = models.BooleanField(default=False)
     publish = models.BooleanField(default=False)
+    type = models.CharField(max_length=128, blank=True, choices=getattr(settings, 'FEEDBACK_TYPES', FEEDBACK_TYPES))
 
     class Meta:
         ordering = ['-timestamp',]
