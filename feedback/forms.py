84a0aef34f8ab187de7e0c2b17c2e79d0e8f2330
Gabriel Hurley <gabriel@strikeawe.com>
2011-06-08 18:29:23 -0700

    Remove the user field from the form to counter-balance making the field editable.

--- feedback/forms.py
+++ feedback/forms.py
@@ -5,4 +5,4 @@ from feedback.models import Feedback
 class FeedbackForm(forms.ModelForm):
     class Meta:
         model = Feedback
-        exclude = ("url", "resolved", "publish",)
+        exclude = ("user", "url", "resolved", "publish",)
